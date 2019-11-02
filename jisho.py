d0f35251fc2595d1adaefd9b55a31cb68ded24c7
Nicolas <nicolas.lindstroem@protonmail.com>
2019-11-02 18:55:11 +0100

    fixed some variable names, added comments

--- jisho.py
+++ jisho.py
@@ -3,20 +3,21 @@
 import sys
 import requests
 import re
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 
 if len(sys.argv) < 2:
     print('Usage: jisho [search terms]')
     exit(1)
 
+# TODO
 display_other_forms = False
 
 search_terms = ' '.join(sys.argv[1:])
 
 source = requests.get('https://jisho.org/search/' + search_terms).text
-jsoup = bs(source, 'lxml')
+soup = BeautifulSoup(source, 'lxml')
 
-matches = jsoup.find('div', id = 'primary')
+matches = soup.find('div', id = 'primary')
 
 if matches is None:
     print('No matches found.')
@@ -27,31 +28,37 @@ result = []
 # Loop through all results
 for match in matches.find_all('div', class_ = 'concept_light clearfix'):
 
+    # Get word + furigana
+    # TODO: fix words with spaced out furigana
     word = match.find('div', class_ = 'concept_light-representation')
     word_furigana = word.find('span', class_ = 'furigana').text.strip()
-    word_kanji = word.find('span', class_ = 'text').text.strip()
+    word_text = word.find('span', class_ = 'text').text.strip()
 
+    # Get all meaning divs, prepary storage array
     meanings = match.find('div', class_ = 'concept_light-meanings')
     meanings_count = 1
-    meanings_array = [word_kanji + (f' ({word_furigana})' if word_furigana else '')]
+    meanings_array = [word_text + (f' ({word_furigana})' if word_furigana else '')]
 
     for meaning in meanings.find_all('div', class_ = 'meaning-definition'):
 
-        # Avoid wiki
+        # TODO: add wiki entries?
         if meaning.find('span', class_ = 'meaning-abstract') is not None:
             continue
 
-        meaning_text = meaning.find('span', class_ = 'meaning-meaning')
-        if not meaning_text:
+        meaning_span = meaning.find('span', class_ = 'meaning-meaning')
+
+        # Ignore notes on okurigana usage
+        if not meaning_span:
             continue
 
-        if meaning_text.find('span', class_ = 'break-unit') is None:
-            meanings_array.append(f'{meanings_count}: {meaning_text.text}')
+        # Separate meaning entries from "Other forms" entry
+        if meaning_span.find('span', class_ = 'break-unit') is None:
+            meanings_array.append(f'{meanings_count}: {meaning_span.text}')
             meanings_count += 1
         elif display_other_forms:
-            meanings_array.append(f'Other forms: {meaning_text.text}')
+            meanings_array.append(f'Other forms: {meaning_span.text}')
 
-    # Print
+    # Append to result array
     if len(meanings_array) > 1:
         result.append('\n'.join(meanings_array))
 
